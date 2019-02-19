62decb0cd0fb7e1994e48161371d87e975de0a6a
suusan11 <gawow11@gmail.com>
2019-02-19 15:20:39 -0800

    create resources page

---
+++ page-resources.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miiiiiiiiiiie
+ * Date: 2019-02-19
+ * Time: 15:17
+ * Template Name: Resources page
+ */
+?>
+
+<!--feature img-->
+<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+    <h1 class="page-title">Resources</h1>
+</section>
+
+<!--main content-->
+<div class="container">
+    <div class="row" id="primary">
+        <div id="content" class="col-sm-12">
+            <section class="main-content">
+                <p class="lead">
+                    Welcome to the Bootstrap to WordPress resources page! On this page, I will provide you with useful resources, plugins, tools, applications, and websites to help make your life as a coder a little bit easier. <strong>I recommend adding this page to your bookmarks for convenient future reference.</strong>
+                </p>
+                <p><em>Please keep in mind that some of the following resources are my affiliate links, and should you decide to make a purchase, I will earn a small commission &mdash; at no extra cost to you! I am recommending these resources only because I have found them to be incredibly helpful. Don&raquo;t spend your money on any products or resources unless you think they will benefit you.</em></p>
+                <hr>
+                <div class="resource-row clearfix">
+                    <div class="resource">
+                        <img src="assets/img/justhost-logo.jpg" alt="JustHost">
+                        <h3><a href="https://bradhussey.ca/justhost">JustHost</a></h3>
+                        <p>When I made my very first website about 7 years ago, I signed up with Justhost &mdash; and I&rsquo;ve never looked back! Since then, I&rsquo;ve built hundreds of websites with Justhost as my only hosting provider. I recommend these guys to all of my clients, my students, my family and friends.</p>
+                        <p>If you&rsquo;re looking to start your very first website, or are looking for a reliable hosting provider &mdash; look no further. You get a free domain name for life, great customer service, unlimited domains, unlimited email accounts, unlimited GBs of space, an anytime money back guarantee and my favourite part: You can set up a blog website in minutes with 1&mdash;click WordPress Install. These guys are awesome!</p>
+                        <a href="https://bradhussey.ca/justhost" class="btn btn-success">Get started with JustHost</a>
+                    </div>
+
+                    <div class="resource">
+                        <img src="assets/img/coda2-logo.jpg" alt="Coda 2">
+                        <h3><a href="https://bradhussey.ca/coda2">Coda 2</a></h3>
+                        <p>If you&rsquo;re serious about coding, or you want to start out with a solid tool to code with, I strongly recommend Coda 2 for Mac. A beautiful User Interface with tabbed navigation, a super sidebar, and a simple layout. The features are outrageous and extensive, with code folding, smart complete, auto indentation, GIT integration, a built-in terminal, iCloud support &mdash; to name a few. The features seem to be endless.</p>
+                        <p>I use Coda 2 for all of my courses &amp; tutorials, and if you decide to use Coda 2 as your editor of choice, it would greatly benefit you when taking my courses. The price of the editor weighs in at $75, but it&raquo;s worth every penny &mdash; in my honest opinion.</p>
+                        <a href="https://bradhussey.ca/coda2" class="btn btn-success">Get started with JustHost</a>
+                    </div>
+
+                    <div class="resource">
+                        <img src="assets/img/dropbox-logo.jpg" alt="Dropbox">
+                        <h3><a href="https://bradhussey.ca/dropbpx">Dropbox</a></h3>
+                        <p>This tool should go without saying. But if I have to explain — you need this tool. I use it 24/7 &mdash; literally. Dropbox is where I store my personal & business related data. It’s accessible anywhere, and regularly backs up my important information.</p>
+                        <p>Because my data is accessible anywhere, Dropbox allows me to work on a client project at home, on the bus, in a coffee shop, or at a friend’s house. It&rsquo;s truly a gem of a tool, and I think you&rsquo;ll believe me.</p>
+                        <a href="https://bradhussey.ca/dropbpx" class="btn btn-success">Get started with JustHost</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</div>
