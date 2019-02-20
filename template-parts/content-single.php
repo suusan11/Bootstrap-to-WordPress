1de7bed1c7852a3fe8fa3dc899a4115c429746b3
suusan11 <gawow11@gmail.com>
2019-02-19 18:08:21 -0800

    modify single post page

---
+++ template-parts/content-single.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miiiiiiiiiiie
+ * Date: 2019-02-19
+ * Time: 18:02
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <?php
+        if ( is_singular() ) :
+            the_title( '<h3 class="entry-title">', '</h3>' );
+        else :
+            the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+        endif;
+
+        if ( 'post' === get_post_type() ) : ?>
+
+            <div class="post-details">
+                <i class="fa fa-user"></i><?php the_author(); ?>
+                <i class="fa fa-clock-o"></i><time><?php the_date(); ?></time>
+                <i class="fa fa-folder"></i><?php the_category(', '); ?>
+                <i class="fa fa-tags"></i><?php the_tags(); ?>
+                <div class="post-comments-badge">
+                    <a href="<?php comment_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+                </div>
+
+                <?php edit_post_link( 'Edit', '<div><i class="fa fa-pencil"></i>', '</div>' ); ?>
+            </div>
+
+        <?php endif; ?>
+    </header><!-- .entry-header -->
+
+    <?php if( has_post_thumbnail() ) { ?>
+        <div class="post-img">
+            <?php the_post_thumbnail(); ?>
+        </div>
+    <?php } ?>
+
+    <div class="post-">body
+        <?php the_content(); ?>
+    </div>
+</article>
